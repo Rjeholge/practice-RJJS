9ebf802b1074c1b9f42a9ba6742ea1f1a5486056
Rjeholge <111187701+Rjeholge@users.noreply.github.com>
2022-12-22 14:25:40 +0000

    [git init]

---
+++ new.js
@@ -0,0 +1,19 @@
+// let hello = 'Hello World';
+// alert('Hello World');
+// let int1 = 1;
+// alert(int1);
+// alert(4 + 3);
+// alert('Hello' + 'World');
+// let str1 = 'Hello';
+// let str2 = 'World!!';
+// alert(str1 + str2);
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
