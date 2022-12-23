c8c2b9e4584b868b409d0c4a3ba8e4c7ab5c5ece
Rjeholge <111187701+Rjeholge@users.noreply.github.com>
2022-12-23 14:11:40 +0000

    2

--- new.js
+++ new.js
@@ -7,13 +7,29 @@
 // let str1 = 'Hello';
 // let str2 = 'World!!';
 // alert(str1 + str2);
-let orange = 100;
-let apple = 120;
 
-if(orange < apple){
-  alert('みかんの値段がりんごより安い');
-} else if(orange == apple){
-  alert('みかんとりんごが同じ値段');
-} else{
-  alert('みかんの値段がりんごより高い');
+// 条件分岐 //
+
+// let orange = 100;
+// let apple = 120;
+
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// } else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// } else{
+//   alert('みかんの値段がりんごより高い');
+// }
+
+// 繰り返し処理//
+
+let max = 1000;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num*6;
+  count = count+1;
 }
+
+alert('6を掛け続けて' + max + 'を越えるのに必要だった回数は' + count + '回です');
